39edae969a4d9de3c6065f671693ba4f04d560c2
Ashish <savitayadavmansarover@gmail.com>
2023-06-27 15:08:12 +0530

    done with mongodb basics

--- index.js
+++ index.js
@@ -1,36 +1,76 @@
 import express from "express";
 const app = express();
 import path from 'path';
+import mongoose from "mongoose";
+//db name boo hoga jis database mai daalna hai ...agr  nhi hoga to fir bnjaiga 
+mongoose.connect('mongodb://localhost:27017', { dbName: "backendashish", }).then(() => { console.log("data base connected") }).catch((err) => { console.log(err) });
+
+const messageSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+//it is the name of the collection that is to be created and the schema which we are using
+const Messge = mongoose.model("messageofashishes", messageSchema);
 
 
-const users = [];
-/* to access form  we need a middleware*/
+
+
+
+/* to access form  response data we need a middleware*/
 app.use(express.urlencoded({ extended: true }))
 
-//setting up view engine
+//setting up view engine--> jissai hmlog views baale ko normal address sai bhi excess kr skai
 app.set("view engine", "ejs");
 
 
+// the starting point of the page
 app.get('/', (req, res) => {
     res.render("index");
+});
+
+
+
+
+app.get('/getdata', async (req, res) => {
+    res.send("data is this");
+    const users = await Messge.find({});  // to get data
+    console.log(users);
+});
+
+app.get('/insertdata', (req, res) => {
+    const messageData = {
+        name: "Abhi",
+        email: "bt2ewfwe@gmail.com"
+    };
+    Messge.create(messageData).then(() => {
+        res.send("i will teach u mongodb")
+        console.log("DAta inserted");
+    })
 })
 
-app.get('/giveusers', (req, res) => {
-    res.json({ users, });
-})
-app.get('/success', (req, res) => {
-    res.render("success");
+//using await it is usually practice in the market
+app.get('/getdataasynce', async (req, res) => {
+    const messageData = {
+        name: "Ashish",
+        email: "bt2ewf2352443252we@gmail.com"
+    };
+    await Messge.create(messageData);
+    res.send("i will teach u mongodb by async");
+});
+
+
+//it the place where form is directing us
+app.post('/contactus', async (req, res) => {
+    const messageData = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+    await Messge.create(messageData);
+    res.send("data inserted succesfully")
+
 })
 
-app.post('/contactus', (req, res) => {
-    console.log(req.body);
-    users.push({
-        Username: req.body.name,
-        Useremail: req.body.email
-    });
-    res.redirect("/success");
-
-})
 
 
 app.listen(5000, () => {

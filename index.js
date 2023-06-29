38fae7388de10d3cf233b5000a830dcf7fd3e151
Ashish <savitayadavmansarover@gmail.com>
2023-06-29 15:52:25 +0530

    first done with handler also known as middle ware , done with jwt protection of token and checking if user exist or not

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 const app = express();
-
+import jwt from 'jsonwebtoken';
 //setting up view engine
 app.set("view engine", "ejs");
 
@@ -27,9 +27,18 @@ const Userschema = mongoose.Schema({
 const UserModel = mongoose.model("Userdata", Userschema);
 
 //it is a middleware or we can say it is a handler .... 
-const isAuthenticated = (req, res, next) => {
+const isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
     if (token) {
+
+        //decoding data with the help of jwt
+        const decodeddata = jwt.verify(token, "qwerty");
+
+
+        //for searching and getting data 
+        req.user = await UserModel.findById(decodeddata._id);
+        console.log(decodeddata);
+
         next();
     }
     else {
@@ -40,28 +49,51 @@ const isAuthenticated = (req, res, next) => {
 
 
 app.get("/", isAuthenticated, (req, res) => {
-    res.render("logout");
+    console.log(req.user);
+    res.render("logout", { name: req.user.name });
 });
 
 app.post("/login", async (req, res) => {
 
+    /*  to avoid insertion of same user*/
     const { name, email } = req.body;
+    const userdata = await UserModel.findOne({ email });
+    if (!userdata) {
+        res.send("Register First");
+        return
+    }
+
+
     console.log(name, email);
     const newuserdata = await UserModel.create({
         name, email
-    }).then(() => {
-        console.log("successfully saved to data base");
     });
+    console.log("successfully saved to data base");
 
-
-    res.cookie("token", newuserdata, {
+    // qwerty is the security key here
+    const token = jwt.sign({ _id: newuserdata._id }, "qwerty");
+    console.log(token);
+    res.cookie("token", token, {
         httpOnly: true, // for security purpose ..so that client can not excess it
         expires: new Date(Date.now() + 120 * 1000),
     });
-
     res.redirect("/");
+
 });
+
+
+
+
+
+
+
+
+
+
 app.get('/logout', (req, res) => {
+
+
+
     res.cookie("token", null, {
         httpOnly: true, // for security purpose ..so that client can not excess it
         expires: new Date(Date.now()),
@@ -69,18 +101,6 @@ app.get('/logout', (req, res) => {
     });
     res.redirect("/");
 });
-app.get('/cookiesdata', (req, res) => {
-    /**cookies is a middle ware function we can not use it directly...we requre cookie parser for it */
-    const data = req.cookies;
-    console.log(data);
-    res.send(data);
-})
-
-
-
-
-
-
 
 app.listen(5000, () => {
     console.log("Server connected");

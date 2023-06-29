a6de5226996c3ac3a9bb95eaace0e04029891918
Ashish <savitayadavmansarover@gmail.com>
2023-06-29 15:01:11 +0530

    done till sending data to monoDb and checking token...but giving value to token is not working

--- index.js
+++ index.js
@@ -1,45 +1,67 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 const app = express();
-
 
 //setting up view engine
 app.set("view engine", "ejs");
 
-
+/* to access form  we need a middleware*/
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 
 
+mongoose.connect('mongodb://localhost:27017', { dbName: "backendashish", }).then(() => { console.log("data base connected") }).catch((err) => { console.log(err) });
+
+
+
+const Userschema = mongoose.Schema({
+    name: String,
+    email: String
+});
 
 
 
 
 
-app.get("/", (req, res) => {
+const UserModel = mongoose.model("Userdata", Userschema);
 
+//it is a middleware or we can say it is a handler .... 
+const isAuthenticated = (req, res, next) => {
     const { token } = req.cookies;
     if (token) {
-        res.render("logout");
+        next();
     }
     else {
         res.render("login");
     }
+}
+
+
+
+app.get("/", isAuthenticated, (req, res) => {
+    res.render("logout");
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
 
-    res.cookie("token", "hiashish", {
+    const { name, email } = req.body;
+    console.log(name, email);
+    const newuserdata = await UserModel.create({
+        name, email
+    }).then(() => {
+        console.log("successfully saved to data base");
+    });
+
+
+    res.cookie("token", newuserdata, {
         httpOnly: true, // for security purpose ..so that client can not excess it
         expires: new Date(Date.now() + 120 * 1000),
     });
+
     res.redirect("/");
 });
-
-
-
-
 app.get('/logout', (req, res) => {
-
     res.cookie("token", null, {
         httpOnly: true, // for security purpose ..so that client can not excess it
         expires: new Date(Date.now()),
@@ -47,8 +69,6 @@ app.get('/logout', (req, res) => {
     });
     res.redirect("/");
 });
-
-
 app.get('/cookiesdata', (req, res) => {
     /**cookies is a middle ware function we can not use it directly...we requre cookie parser for it */
     const data = req.cookies;

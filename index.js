7c59c65e47c53bbda60cc6292948acd092a17363
Ashish <savitayadavmansarover@gmail.com>
2023-07-01 17:49:14 +0530

    finally done with a good login,logout,registration Page

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import express, { urlencoded } from 'express';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
 const app = express();
-import bcrypt from "bcrypt";
 
 // to avoid hacking 
 import jwt from 'jsonwebtoken';
-//setting up view engine
-app.set("view engine", "ejs");
-
-/* to access form  we need a middleware*/
-app.use(express.urlencoded({ extended: true }))
+mongoose.set('strictQuery', true);
 app.use(cookieParser());
 
-
+//db name boo hoga jis database mai daalna hai ...agr  nhi hoga to fir bnjaiga 
 mongoose.connect('mongodb://localhost:27017', { dbName: "backendashish", }).then(() => { console.log("data base connected") }).catch((err) => { console.log(err) });
-
-
-
-const Userschema = mongoose.Schema({
+const Userschema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String,
-
+    phone: Number,
+    password: String
 });
 
 
+const UserModel = mongoose.model("formHandling", Userschema);
 
-
-
-const UserModel = mongoose.model("Userdata", Userschema);
-
-
-
-
+//setting up view engine
+app.set('view engine', 'ejs');
+/* to access form  we need a middleware*/
+app.use(express.urlencoded({ extended: true }))
 //it is a middleware or we can say it is a handler .... 
 const isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
@@ -45,52 +36,47 @@ const isAuthenticated = async (req, res, next) => {
         next();
     }
     else {
-        res.redirect("/login");
+        return res.redirect("/login");
     }
 }
 
-
-//done
-app.get("/", isAuthenticated, (req, res) => {
+app.get('/', isAuthenticated, (req, res) => {
     res.render("logout");
+
 });
 
-//done
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
 app.get('/register', (req, res) => {
     res.render('register');
 })
-
-//done
-app.get('/login', (res, req) => {
-    req.render('login');
-})
-
-
-
-// done
 app.get('/logout', (req, res) => {
     res.cookie("token", null, {
         httpOnly: true, // for security purpose ..so that client can not excess it
         expires: new Date(Date.now()),
     });
     res.redirect("/");
-});
+})
 
 
 
-app.post("/login", async (req, res) => {
-    /*  to avoid insertion of same user*/
-    const { password, email } = req.body;
-    const userdata = await UserModel.findOne({ email });
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email) {
+        return res.render("login", { message: "PLEASE ENTER A VALID EMAIL" });
+
+    }
+    const userdata = await UserModel.findOne({ email: email });
     if (!userdata) {
-        return res.redirect("/register");
+        return res.redirect('/register');
     }
 
-
-    const isMatch = await bcrypt.compare(password, userdata.password);
-
-    console.log(password);
-    if (isMatch) {
+    const ismatch = await bcrypt.compare(password, userdata.get("password"));
+    console.log("ismatch", ismatch);
+    if (ismatch) {
 
         const token = jwt.sign({ _id: userdata._id }, "qwerty");
 
@@ -98,28 +84,26 @@ app.post("/login", async (req, res) => {
             httpOnly: true,
             expires: new Date(Date.now() + 60 * 1000),
         });
-        return res.render('logout', { name: userdata.name });
+        res.render('logout', { name: userdata.name, email: userdata.email, phone: userdata.phone });
     }
     else {
-        res.render('login', { message: "Wrong Password" });
-        return res.redirect('/login');
+        res.render("login", { message: "PLEASE ENTER A CORRECT PASSWORD" });
     }
-});
-
-
+})
 app.post('/register', async (req, res) => {
-    /*  to avoid insertion of same user*/
-    const { name, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const userdata = await UserModel.findOne({ email });
+    const { name, email, phone, password } = req.body;
+
+    const userdata = await UserModel.findOne({ email: email });
     if (userdata) {
-        return res.redirect('/login');
+        return res.render('login', { email: req.body.email });
     }
-    const newuserdata = await UserModel.create({
-        name, email, password: hashedPassword,
-    });
-    res.redirect("/");
+    else {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const data = { name, email, phone, password: hashedPassword };
+        await UserModel.create(data);
+        res.redirect('/');
+    }
 })
 app.listen(5000, () => {
-    console.log("Server connected");
-})
+    console.log("server created");
+})  
